8443b5020cbecc669c4c02833b8799c9c3957e1b
平岡　和也 <der.starn.himmel@gmail.com>
2022-06-17 09:30:31 +0000

    ユーザー情報更新ページのレイアウト完成

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   # ログイン機能
-  get  '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
   resources :users
